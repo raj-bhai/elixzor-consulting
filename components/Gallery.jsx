935fba8665e1d196ed45e1f3d4d5c9851be20925
Rajkiran Kalowar <rajkiranjnv1@gmail.com>
2024-11-22 20:12:33 +0530

    silver-button and gallery

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -6,82 +6,107 @@ const Gallery = ({scrollToBookYourCall}) => {
         {
           id: 1,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999951/elixzor-consulting/testimonials/4_pzneyo.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284455/elixzor-consulting/testimonials-edited/edited13_eesdak.png"
         },
         {
           id: 2,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999950/elixzor-consulting/testimonials/1_euefxw.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284455/elixzor-consulting/testimonials-edited/edited10_kw1ip6.png"
         },
         {
           id: 5,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999950/elixzor-consulting/testimonials/5_siwzzn.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284455/elixzor-consulting/testimonials-edited/edited14_zvlsfv.png"
         },
         {
           id: 6,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999950/elixzor-consulting/testimonials/3_j6ph2a.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284455/elixzor-consulting/testimonials-edited/edited11_cn6qaj.png"
         },
         {
           id: 7,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999950/elixzor-consulting/testimonials/9_2_bl3miu.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284455/elixzor-consulting/testimonials-edited/edited9_td4ydu.png"
         },
         {
           id: 8,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999949/elixzor-consulting/testimonials/13_1_lglz69.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284455/elixzor-consulting/testimonials-edited/edited3_ff9v8c.png"
         },
         {
           id: 9,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999949/elixzor-consulting/testimonials/10_1_nkmsff.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284455/elixzor-consulting/testimonials-edited/edited12_rslaza.png"
         },
         {
           id: 10,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999949/elixzor-consulting/testimonials/11_1_afl7db.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284454/elixzor-consulting/testimonials-edited/edited7_igscsr.png"
         },
         {
           id: 11,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999949/elixzor-consulting/testimonials/15_1_ymamgk.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284453/elixzor-consulting/testimonials-edited/edited15_rdl2uq.png"
         },
         {
           id: 12,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999949/elixzor-consulting/testimonials/6_uh1tdv.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284454/elixzor-consulting/testimonials-edited/edited5_je9wx2.png"
         },
         {
           id: 13,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999949/elixzor-consulting/testimonials/7_e9hz84.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284454/elixzor-consulting/testimonials-edited/edited8_wyindz.png"
         },
         {
           id: 14,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999948/elixzor-consulting/testimonials/8_gxak40.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284454/elixzor-consulting/testimonials-edited/edited6_dxceja.png"
         },
         {
           id: 15,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999948/elixzor-consulting/testimonials/14_1_lgohqy.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284454/elixzor-consulting/testimonials-edited/edited4_zuo0fu.png"
         },
         {
           id: 16,
           image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999948/elixzor-consulting/testimonials/16_1_xds9vc.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284453/elixzor-consulting/testimonials-edited/edited16_bt2yrv.png"
+        },
+        {
+          id: 17,
+          image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999948/elixzor-consulting/testimonials/16_1_xds9vc.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284453/elixzor-consulting/testimonials-edited/edited2_tc55cu.png"
+        },
+        {
+          id: 18,
+          image: "https://res.cloudinary.com/dm9iuudyc/image/upload/v1731999948/elixzor-consulting/testimonials/16_1_xds9vc.png",
+          imageCropped : "https://res.cloudinary.com/dm9iuudyc/image/upload/v1732284453/elixzor-consulting/testimonials-edited/edited1_jvnhmi.png"
         },
       ]
 
   return (
     <div className="container mx-auto p-6 font-sans text-white ">
-    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-2">
-      {imageSlides.map((slide) => (
-        <div key={slide.id} className="relative w-full h-80 overflow-hidden rounded-lg shadow-lg">
-          {slide.image.endsWith(".mp4") ? (
-            <video controls className="w-full h-full object-cover rounded-lg">
-              <source src={slide.image} type="video/mp4" />
-            </video>
-          ) : (
-            <Image
-              src={slide.image}
-              alt={`Slide ${slide.id}`}
-              layout="fill"
-              objectFit="cover"
-              className="rounded-lg w-full "
-            />
-          )}
-        </div>
-                ))}
-                </div>
+      <div className="flex flex-col gap-8 sm:gap-16">
+        {imageSlides.map((slide) => (
+          <div key={slide.id} className="overflow-hidden rounded-lg shadow-lg w-full">
+            {slide.image.endsWith(".mp4") ? (
+              <video controls className="w-full h-auto object-cover rounded-lg">
+                <source src={slide.imageCropped} type="video/mp4" />
+              </video>
+            ) : (
+              <Image
+                src={slide.imageCropped}
+                alt={`Slide ${slide.id}`}
+                width={1920} // Ensure full-width scaling
+                height={0} // Automatically adjust height based on aspect ratio
+                layout="intrinsic" // Preserves the natural dimensions of the image
+                className="rounded-lg"
+              />
+            )}
+          </div>
+        ))}
+      </div>
                 <CTAButton
             onClick={scrollToBookYourCall}
             text="YES! I WANT TO START"
--- components/MessageSection.jsx
+++ components/MessageSection.jsx
@@ -1,61 +1,91 @@
+import Image from "next/image";
 export default function MessageSection() {
-    return (
-      <section className="p-0 flex flex-col font-sans items-center text-center sm:p-12  text-left w-full mx-auto  ">
-        <p className="text-white text-lg mb-6">Dear aspiring online entrepreneur,</p>
-        
-        <p className="text-white text-lg mb-4">
-          If you want to <strong className="font-semibold">make your first paycheck online with YouTube automation in the next 60 days without putting in the time and efforts</strong>
-        </p>
-        
-        <p className="text-lg text-[#000] mb-4 bg-yellow-200 p-2 inline-block rounded">
-          Then this will be the most effective and successful service you will ever get.
-        </p>
-  
-        <p className="text-white text-lg mb-4">
-          Doesn’t matter even if you&apos;ve never made any money from YouTube.
-        </p>
-  
-        <p className="text-white text-lg mb-4">
-          You can do so with our strategy and proven methods which made our clients millions of dollars.
-        </p>
-  
-        <p className="text-white text-lg mb-4 font-semibold">Here&apos;s why:</p>
-  
-        <p className="text-white text-lg mb-4">
-          My name is Siddharth Paul and a few years ago <strong>I had nothing.</strong>
-        </p>
-  
-        <p className="text-white text-lg mb-4">
-          I was living with my single mom with 2 more siblings and we were in debt of $25,000+ left by our dad when he got separated. We live in India, so that’s 10+ years of GDP per capita…
-        </p>
-        <p className="text-white text-lg mb-4">
-        It was covid so we couldn&apos;t go nowhere so I had just jumped into the online business world, with a dream to make it big online to get us out of that situation.
-        </p>
-        <p className="text-white text-lg mb-4" >
+  return (
+    <section className="p-0 flex flex-col font-sans items-center text-center sm:p-12  text-left w-full mx-auto  ">
+      <div className="w-full flex flex-col md:flex-row justify-center items-center gap-4 md:gap-16 mb-8">
+        <img
+          src="https://res.cloudinary.com/dm9iuudyc/image/upload/v1732285423/elixzor-consulting/Silver-button/IMG_3504_g7tyij.jpg"
+          alt="Image 1"
+          className="rounded-lg w-full max-w-xs"
+        />
+        <img
+          src="https://res.cloudinary.com/dm9iuudyc/image/upload/v1732285422/elixzor-consulting/Silver-button/IMG_3503_oiigol.jpg"
+          alt="Image 2"
+          className="rounded-lg w-full max-w-xs"
+        />
+      </div>
+
+      <p className="text-white text-lg mb-6">
+        Dear aspiring online entrepreneur,
+      </p>
+
+      <p className="text-white text-lg mb-4">
+        If you want to{" "}
+        <strong className="font-semibold">
+          make your first paycheck online with YouTube automation in the next 60
+          days without putting in the time and efforts
+        </strong>
+      </p>
+
+      <p className="text-lg text-[#000] mb-4 bg-yellow-200 p-2 inline-block rounded">
+        Then this will be the most effective and successful service you will
+        ever get.
+      </p>
+
+      <p className="text-white text-lg mb-4">
+        Doesn’t matter even if you&apos;ve never made any money from YouTube.
+      </p>
+
+      <p className="text-white text-lg mb-4">
+        You can do so with our strategy and proven methods which made our
+        clients millions of dollars.
+      </p>
+
+      <p className="text-white text-lg mb-4 font-semibold">Here&apos;s why:</p>
+
+      <p className="text-white text-lg mb-4">
+        My name is Siddharth Paul and a few years ago{" "}
+        <strong>I had nothing.</strong>
+      </p>
+
+      <p className="text-white text-lg mb-4">
+        I was living with my single mom with 2 more siblings and we were in debt
+        of $25,000+ left by our dad when he got separated. We live in India, so
+        that’s 10+ years of GDP per capita…
+      </p>
+      <p className="text-white text-lg mb-4">
+        It was covid so we couldn&apos;t go nowhere so I had just jumped into
+        the online business world, with a dream to make it big online to get us
+        out of that situation.
+      </p>
+      <p className="text-white text-lg mb-4">
         I was 16 and had no one to guide me…
-        </p>
-        <p className="text-white text-lg mb-4" >
-        It was also at that time that I decided to start freelancing as an editor and drop out of high school.
-        </p>
-        <p className="text-white text-lg mb-4" >
-        I basically had all odds stacked against me. 
-        </p>
-        <p className="text-white text-lg mb-4" >
-        I went on to spend months trying to find the perfect online business model…Sales funnels, Dropshipping, Affiliate marketing, Online apps, You name it…
-        </p>
-        <p className="text-white text-lg mb-4" >
+      </p>
+      <p className="text-white text-lg mb-4">
+        It was also at that time that I decided to start freelancing as an
+        editor and drop out of high school.
+      </p>
+      <p className="text-white text-lg mb-4">
+        I basically had all odds stacked against me.
+      </p>
+      <p className="text-white text-lg mb-4">
+        I went on to spend months trying to find the perfect online business
+        model…Sales funnels, Dropshipping, Affiliate marketing, Online apps, You
+        name it…
+      </p>
+      <p className="text-white text-lg mb-4">
         I&apos;ve tried them all, <strong> but nothing worked</strong>.
-        </p>
-        <p className="text-white text-lg mb-4" >
-        All I&apos;d do was spend 10 to 12h every single day bumping my head against the wall trying to make this online business thing work…
-        </p>
-        <p className="text-white text-lg mb-4" >
+      </p>
+      <p className="text-white text-lg mb-4">
+        All I&apos;d do was spend 10 to 12h every single day bumping my head
+        against the wall trying to make this online business thing work…
+      </p>
+      <p className="text-white text-lg mb-4">
         Only to get hit with multiple massive failures every time.
-        </p>
-        <p className="text-white text-lg mb-4" >
+      </p>
+      <p className="text-white text-lg mb-4">
         It was like that for a long time…
-        </p>
-      </section>
-    );
-  }
-  
+      </p>
+    </section>
+  );
+}
